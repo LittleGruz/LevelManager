b1a0984920142937a278638e07e21a65cbca65c4
littlegruz <littlegruz@gmail.com>
2013-05-30 08:13:07 +1000

    OP perms for commands

--- src/com/littlegruz/levelmanager/commands/LevelRequirements.java
+++ src/com/littlegruz/levelmanager/commands/LevelRequirements.java
@@ -16,38 +16,45 @@ public class LevelRequirements implements CommandExecutor{
    @Override
    public boolean onCommand(CommandSender sender, Command cmd,
          String commandLabel, String[] args){
-      if(cmd.getName().compareToIgnoreCase("spelllevel") == 0){
-         if(args.length == 2){
-            try{
-            plugin.getLevelConfig().set(args[0], Integer.parseInt(args[1]));
-            
-            sender.sendMessage("Spell \"" + args[0] + "\" level requirement is now " + args[1]);
-            plugin.saveLevelConfig();
-            
-            return true;
-            }catch(NumberFormatException e){
-               sender.sendMessage("Not a valid number");
+      if(sender.hasPermission("manager.commands")){
+         if(cmd.getName().compareToIgnoreCase("spelllevel") == 0){
+            if(args.length == 2){
+               try{
+               plugin.getLevelConfig().set(args[0], Integer.parseInt(args[1]));
+               
+               sender.sendMessage("Spell \"" + args[0] + "\" level requirement is now " + args[1]);
+               plugin.saveLevelConfig();
+               
+               return true;
+               }catch(NumberFormatException e){
+                  sender.sendMessage("Not a valid number");
+               }
             }
+            else
+               sender.sendMessage("Wrong number of arguments");
          }
-         else
-            sender.sendMessage("Wrong number of arguments");
-      }
-      else if(cmd.getName().compareToIgnoreCase("levelcap") == 0){
-         if(args.length == 1){
-            try{
-            plugin.getLevelConfig().set("max_level", Integer.parseInt(args[0]));
-            
-            sender.sendMessage("Level cap is now " + args[0]);
-            plugin.saveLevelConfig();
-            
-            return true;
-            }catch(NumberFormatException e){
-               sender.sendMessage("Not a valid number");
+         else if(cmd.getName().compareToIgnoreCase("levelcap") == 0){
+            if(args.length == 1){
+               try{
+               plugin.getLevelConfig().set("max_level", Integer.parseInt(args[0]));
+               
+               sender.sendMessage("Level cap is now " + args[0]);
+               plugin.saveLevelConfig();
+               
+               return true;
+               }catch(NumberFormatException e){
+                  sender.sendMessage("Not a valid number");
+               }
             }
+            else
+               sender.sendMessage("Wrong number of arguments");
          }
-         else
-            sender.sendMessage("Wrong number of arguments");
       }
+      else{
+         sender.sendMessage("Insufficient permissions");
+         return true;
+      }
+      
       return false;
    }
 
